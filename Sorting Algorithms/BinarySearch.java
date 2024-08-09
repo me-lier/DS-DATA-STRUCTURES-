7b3d0b0a229c05098194f17e512b9f0e771d3605
Srinivas Deekonda <126309422+me-lier@users.noreply.github.com>
2024-08-09 17:55:46 +0530

    Create BinarySearch.java

---
+++ Sorting Algorithms/BinarySearch.java
@@ -0,0 +1,48 @@
+package practise;
+import java.util.*;
+public class BinarySearch {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] +",");
+        }
+        System.out.println("\n");
+        int key = sc.nextInt();
+        sc.close();
+
+        int res = BinarySearch(arr, n, key);
+        if(res!=-1){
+            System.out.println(res);
+        }else{
+            System.out.println("Not found in Array");
+        }
+
+        
+
+    }
+
+    public static int BinarySearch(int[] arr, int n, int key){
+        int low = 0, high = n-1;
+        while(low<high){
+            int mid = (low + high) / 2;
+            if(arr[mid] == key){
+                return mid;
+            }
+            if(arr[mid] > key){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    
+}
+
